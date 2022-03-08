10a93ace907e2c71cb6c5a879e13375228bf25a2
Vengzz <joaovvanz@gmail.com>
2022-03-07 21:48:40 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroProgama.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+
+import model.Gato;
+
+public class PrimeiroProgama {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello world");*/
+    }
+
+}
